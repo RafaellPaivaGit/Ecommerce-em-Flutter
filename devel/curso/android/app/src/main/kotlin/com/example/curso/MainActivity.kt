4aa5540390000d02ee1568bf6f23967548fea8bf
Rafaell Paiva <rfaellperera@gmail.com>
2025-09-01 16:57:55 -0300

    Delete devel/curso directory

--- devel/curso/android/app/src/main/kotlin/com/example/curso/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.curso
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- devel/curso/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  //runApp(const MyApp());
-  //
-  runApp(MaterialApp(home: Text('aaaa')));
-}
