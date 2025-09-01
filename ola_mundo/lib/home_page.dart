710fa8cadec254777af04578467a3b92cfa1da2e
RafaellPaivaGit <rfaellperera@gmail.com>
2025-09-01 16:52:42 -0300

    Resolvendo conflitos

--- ola_mundo/lib/home_page.dart
+++ ola_mundo/lib/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ola_mundo/categorieswidget.dart';
-import 'package:ola_mundo/homeappbar.dart';
 import 'package:ola_mundo/itemwidget.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,10 +13,69 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 1],
+                  colors: [
+                    Color.fromARGB(255, 34, 1, 255),
+                    Color.fromARGB(255, 81, 29, 214),
+                  ],
+                ),
+              ),
+              currentAccountPicture: ClipRRect(
+                borderRadius: BorderRadius.circular(40),
+                child: Image.network(
+                  'https://pbs.twimg.com/media/ExUgVx7WQAMcTV9.jpg',
+                ),
+              ),
+              accountName: Text('Rafaell Paiva'),
+              accountEmail: Text('rafaell@email.com.br'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Inicio'),
+              subtitle: Text('Tela de inicio'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/home');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.deck),
+              title: Text('Detail'),
+              subtitle: Text('Detalhes'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/detail');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Logout'),
+              subtitle: Text('Finalizar sessão'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text('DevHub Shop'),
+        titleTextStyle: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+          color: Color.fromARGB(255, 34, 1, 255),
+        ),
+      ),
+
       body: SafeArea(
         child: ListView(
           children: [
-            HomeAppBar(),
             Container(
               //height: 500,
               padding: EdgeInsets.only(top: 15),
